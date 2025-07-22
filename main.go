15cf06f8531f5b622e72590c7eb70ef0752852db
Caio Carvalho <caiocarvalho.py@gmail.com>
2025-07-22 05:56:46 -0400

    Refactor main.go to implement GameState and Question structures with initialization logic

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+type Question struct {
+	Text    string
+	Options []string
+	Awnser  int
+}
+
+type GameState struct {
+	Name      string
+	Points    string
+	Questions []Question
+}
+
+func (g *GameState) Init() {
+	fmt.Println("Seja bem vindo(a)! ao quiz")
+	fmt.Println("Escreva o seu nome:")
+	reader := bufio.NewReader(os.Stdin)
+	name, err := reader.ReadString('\n')
+
+	if err != nil {
+		panic("Erro ao ler a string!")
+	}
+
+	g.Name = name
+	fmt.Printf("Vamos ao jogo %s", g.Name)
+}
+
 func main() {
-	fmt.Println("At the risk of feeling dumb, check in!")
+	game1 := &GameState{}
+	game1.Init()
 }
